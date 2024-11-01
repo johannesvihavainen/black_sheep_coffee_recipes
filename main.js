6e347a16cd8c904263f370371a42e6c650a17b17
Johannes Vihavainen <johannesvihavainen@yahoo.com>
2024-11-01 00:24:24 -0500

    added search bar functionality for input; it will filter each letter/word with the innerHTML of a class and update the styles.css accordingly

---
+++ main.js
@@ -0,0 +1,29 @@
+var getDrinkTitle = document.querySelectorAll('.drink-title');
+var getInput = document.querySelector('.search-box');
+
+// for loop will convert all drink titles to UPPERCASE...
+for (var i = 0; i < getDrinkTitle.length; i++) {
+    getDrinkTitle[i].innerHTML = getDrinkTitle[i].innerHTML.toUpperCase();
+    console.log(getDrinkTitle[i]);
+};
+
+getInput.addEventListener('input', function () {
+    var inputValue = getInput.value.toUpperCase();
+
+    for (var i = 0; i < getDrinkTitle.length; i++) {
+        var drinkTitle = getDrinkTitle[i].innerHTML.toUpperCase();
+
+
+
+        if (drinkTitle.includes(inputValue)) {
+            getDrinkTitle[i].parentElement.style.display = "block";
+        }
+        else {
+            getDrinkTitle[i].parentElement.style.display = "none";
+        }
+    }
+})
+
+
+
+
